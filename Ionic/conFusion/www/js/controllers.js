88292216145b9e8cf99cf8d2ef37a0ea5e428630
dmsuzuki <dayanasuzuki@gmail.com>
2017-01-29 19:17:22 -0200

    2.2 Ionic Lists - Advanced Features Part 1

--- Ionic/conFusion/www/js/controllers.js
+++ Ionic/conFusion/www/js/controllers.js
@@ -72,7 +72,7 @@ angular.module('conFusion.controllers', [])
 
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -81,7 +81,7 @@ angular.module('conFusion.controllers', [])
     $scope.showMenu = false;
     $scope.message = "Loading ...";
 
-    menuFactory.getDishes().query(
+    $scope.dishes = menuFactory.getDishes().query(
         function(response) {
             $scope.dishes = response;
             $scope.showMenu = true;
@@ -114,6 +114,12 @@ angular.module('conFusion.controllers', [])
 
     $scope.toggleDetails = function() {
         $scope.showDetails = !$scope.showDetails;
+    };
+
+    $scope.addFavorite = function (index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
     };
 }])
 
