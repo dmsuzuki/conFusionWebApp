789565ed46bf4d87730756bdc2a4d1dfdb83926c
dmsuzuki <dayanasuzuki@gmail.com>
2017-02-05 23:50:57 -0200

    3.3 Using Local Storage

--- Ionic/conFusion/www/js/services.js
+++ Ionic/conFusion/www/js/services.js
@@ -40,13 +40,30 @@ angular.module('conFusion.services', ['ngResource'])
           favorites.splice(i, 1);
         }
       }
-    }
+    };
 
     favFac.getFavorites = function () {
       return favorites;
     };
 
     return favFac;
+  }])
+
+  .factory('$localStorage', ['$window', function ($window) {
+    return {
+      store: function (key, value) {
+        $window.localStorage[key] = value;
+      },
+      get: function (key, defaultValue) {
+        return $window.localStorage[key] || defaultValue;
+      },
+      storeObject: function (key, value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject: function (key,defaultValue) {
+        return JSON.parse($window.localStorage[key] || defaultValue);
+      }
+    }
   }])
 
 ;
